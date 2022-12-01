862f7fd5776abd2107908d6ecb5958e04c801d12
Noah Schultz <nschult1@student.fitchburgstate.edu>
2022-12-01 14:07:48 -0500

    changed thing

--- Fractals.jl
+++ Fractals.jl
@@ -1,9 +1,10 @@
 module Fractals
 
-import Base.show
+import Base: show#, iterate
 
 export FractalView, iterate, leavingNumber, inMandelbrot
 
+# has a max and min coplex number, and integers for dimensions 
 ```
 This is a helpful comment
 ```
@@ -16,36 +17,46 @@ struct FractalView
     height::Integer
 end
 
-#iterate ( increment by c )
+#iterate ( function, inital point, number of increments)
 ```
 This is a helpful comment
 ```
-function iterate(c::Complex)
+function iterateFunction(f::Function, initial::Complex, i::Integer)
     
 end
 
 #leavingNumber ( how many iterations until number is more than 2 -> goes to infinity )
+# optional parameter = number of iterations to test
+# optional paramter = initial point, default 0 + 0im
 ```
 This is a helpful comment
 ```
-function leavingNumber(i::Integer)
+function leavingNumber(i::Integer = 100, inital::Complex = 0 + 0im)
+    if (initial += 0 + 1im) < 2
+        # recursive? 
+        leavingNumber(i-=1, inital)
+    end
     
 end
 
 #inMandelbrot ( iterate number a ton, and if not more than 2, then it is bounded )
+# by default iterate a 100 times
 ```
 This is a helpful comment
 ```
-function inMandelbrot()
-    j = 1000
+function inMandelbrot(c::Complex, j::Integer = 100)
+    #j = 100
 
-    for i in 1:j
-       #if iterate() > 2
+    #for i in 1:j
+       #if iterate(c) > 2
+            #print( "Leaves the Mandelbrot set after ", i, " iterations" )
             #return false
         #end
-    end
+    #end
     
-    return true
+    leavingNumber(j, c)
+    
+    #return true
 end
 
 end
